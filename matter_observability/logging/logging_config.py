3db7f9e96dcf37ed0451320bd390ce253fc3ecf6
Tomer Sasson <tomer@thisismatter.com>
2023-04-04 18:21:55 +0200

    fix logging config and metric names

--- matter_observability/logging/logging_config.py
+++ matter_observability/logging/logging_config.py
@@ -2,7 +2,7 @@ from matter_observability.config import Config
 
 LOGGING_CONFIG = {
     "version": 1,
-    "filters": {"request_id": {"()": "app.utils.request_id.RequestIdLogFilter"}},
+    "filters": {"request_id": {"()": "matter_observability.fastapi.request_id.RequestIdLogFilter"}},
     "formatters": {
         "standard": {
             "format": "[%(asctime)s] [%(levelname)s] [%(name)s] [%(request_id)s] [%(filename)s:%(funcName)s:%(lineno)s] - %(message)s"
@@ -27,22 +27,22 @@ LOGGING_CONFIG = {
     "loggers": {
         "root": {
             "handlers": ["default"],
-            "level": Config.SERVER_LOG_LEVEL,
+            "level": Config.SERVER_LOG_LEVEL.upper(),
             "propagate": False,
         },
         "uvicorn": {
             "handlers": ["no_request_id"],
-            "level": Config.SERVER_LOG_LEVEL,
+            "level": Config.SERVER_LOG_LEVEL.upper(),
             "propagate": False,
         },
         "uvicorn.access": {
             "handlers": ["default"],
-            "level": Config.SERVER_LOG_LEVEL,
+            "level": Config.SERVER_LOG_LEVEL.upper(),
             "propagate": False,
         },
         "sqs-consumer": {
             "handlers": ["default"],
-            "level": Config.SERVER_LOG_LEVEL,
+            "level": Config.SERVER_LOG_LEVEL.upper(),
             "propagate": False,
         },
     },
