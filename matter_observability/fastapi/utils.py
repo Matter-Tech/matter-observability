27e70051aa0a437548e5a86c993b045af1983d73
lukasz-matter <lukasz@thisismatter.com>
2024-04-23 14:56:19 +0200

    Reinstate skip_paths for instrumentation

--- matter_observability/fastapi/utils.py
+++ matter_observability/fastapi/utils.py
@@ -1,19 +1,17 @@
 from fastapi import FastAPI
 from prometheus_fastapi_instrumentator import Instrumentator
 
-from matter_observability.config import Config
-from matter_observability.exceptions import MisConfigurationError
-
 from .request_id import process_request_id
 
 
-def configure_middleware(app: FastAPI) -> None:
+def configure_middleware(app: FastAPI, skip_paths: list[str] | None = None) -> None:
     metrics_path = "/internal/metrics"
+    skip_paths = skip_paths or [metrics_path]
 
     app.middleware("http")(process_request_id)
 
-    if Config.ENABLE_METRICS:
-        if bool(Config.INSTANCE_NAME) is False:
-            raise MisConfigurationError("Environment variable: INSTANCE_NAME is not valid")
-
-        Instrumentator().instrument(app=app).expose(app=app, endpoint=metrics_path)
+    Instrumentator(
+        excluded_handlers=skip_paths,
+        should_respect_env_var=True,
+        env_var_name="ENABLE_METRICS"
+    ).instrument(app=app).expose(app=app, endpoint=metrics_path)
